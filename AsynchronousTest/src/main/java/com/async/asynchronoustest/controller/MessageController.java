8207d729dc23265c6c17d6971a3c10334473dc26
WontaeSeong <won4932@gmail.com>
2022-01-06 09:57:07 +0900

    async controller

---
+++ AsynchronousTest/src/main/java/com/async/asynchronoustest/controller/MessageController.java
@@ -0,0 +1,70 @@
+package com.async.asynchronoustest.controller;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.async.asynchronoustest.service.MessageService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class MessageController {
+
+	private final MessageService messageService;
+
+	@GetMapping("/message")
+	@ResponseStatus(code = HttpStatus.OK)
+	public void printMessage() {
+		for(int i = 0; i < 100; i++) {
+			messageService.asyncPrint(i + " ");
+
+		}
+	}
+
+	@GetMapping("/future")
+	@ResponseStatus(code = HttpStatus.OK)
+	public void futureMessage() throws Exception {
+		for(int i = 0; i < 10; i++) {
+			Future<String> future = messageService.futurePrint(i + " ");
+			// System.out.println(future.get());
+		}
+	}
+
+	@GetMapping("/listenable-future")
+	@ResponseStatus(code = HttpStatus.OK)
+	public void listenableFutureMessage() throws Exception {
+		for(int i = 0; i < 10; i++) {
+			ListenableFuture<String> future = messageService.listenableFuturePrint(i + " ");
+
+			// future.addCallback(, error -> System.out.println(error.getMessage()));
+		}
+	}
+
+	@GetMapping("/completable-future")
+	@ResponseStatus(code = HttpStatus.OK)
+	public void completableFutureMessage() throws Exception {
+		for(int i = 0; i < 10; i++) {
+			CompletableFuture<String> future = messageService.completableFuturePrint(i + " ");
+
+			future.thenAccept(System.out::println)
+				.exceptionally(error -> {
+					System.out.println(error.getMessage());
+					return null;
+				});
+
+			CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+				messageService.completableFuturePrint(i + " ");
+			});
+			// future.thenRun(() -> {
+			// 	System.out.println("Run Task");
+			// });
+		}
+	}
+}
