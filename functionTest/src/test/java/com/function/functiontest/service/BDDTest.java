a26c9e47ea70b5c30e1762984cfbb5783c619592
WontaeSeong <won4932@gmail.com>
2022-02-17 09:57:07 +0900

    BDD test Code

---
+++ functionTest/src/test/java/com/function/functiontest/service/BDDTest.java
@@ -0,0 +1,14 @@
+package com.function.functiontest.service;
+
+import org.junit.jupiter.api.Test;
+
+public class BDDTest {
+
+	@Test
+	void verifyTest() {
+		// given
+		// when -> given
+		given()
+	}
+	count(distct case when) as 'gfd'
+}
