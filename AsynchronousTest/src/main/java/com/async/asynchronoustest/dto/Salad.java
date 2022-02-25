c2809e879ef4d11709a17b2a81750d9eb349ee89
WontaeSeong <won4932@gmail.com>
2022-02-25 09:57:07 +0900

    Async dto

--- AsynchronousTest/src/main/java/com/async/asynchronoustest/dto/Salad.java
+++ AsynchronousTest/src/main/java/com/async/asynchronoustest/dto/Salad.java
@@ -1,4 +1,16 @@
 package com.async.asynchronoustest.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+@AllArgsConstructor
 public class Salad {
+
+	private String name;
+	private int waitTime;
+
+
 }
