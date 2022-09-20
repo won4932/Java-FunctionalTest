ae5f2a821e08bb7024ae9a1ec7843a7247bddebc
WonTaeSeong <won4932@gmail.com>
2022-09-20 09:51:13 +0900

    java11 test project add

---
+++ java11-test/src/main/java/com/practice/java11test/Java11TestApplication.java
@@ -0,0 +1,13 @@
+package com.practice.java11test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java11TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java11TestApplication.class, args);
+	}
+
+}
---
+++ java11-test/src/test/java/com/practice/java11test/Java11TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.java11test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java11TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
