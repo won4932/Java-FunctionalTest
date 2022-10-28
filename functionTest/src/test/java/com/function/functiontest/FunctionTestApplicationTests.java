f0a14c0aae8cd80fb7adb662bcb9fc89c43982d1
WonTaeSeong <won4932@gmail.com>
2022-10-28 11:11:56 +0900

    각 종 기능 테스트

---
+++ functionTest/src/test/java/com/function/functiontest/FunctionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.function.functiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
