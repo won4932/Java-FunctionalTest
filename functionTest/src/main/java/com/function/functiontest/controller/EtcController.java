8caaafa03c036198a92a4dabd0bebcb46bd099a2
WonTaeSeong <won4932@gmail.com>
2022-11-15 13:19:16 +0900

    system property test

--- functionTest/src/main/java/com/function/functiontest/controller/EtcController.java
+++ functionTest/src/main/java/com/function/functiontest/controller/EtcController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.function.functiontest.service.DateService;
+import com.function.functiontest.service.EtcService;
 
 import lombok.AllArgsConstructor;
 
@@ -17,6 +18,8 @@ public class EtcController {
 
 	private final DateService dateService;
 
+	private final EtcService etcService;
+
 	@GetMapping("/etc/dateTest")
 	public void test() throws ParseException {
 		dateService.dateTimeTest();
@@ -25,5 +28,10 @@ public class EtcController {
 	@GetMapping("/etc/versionTest")
 	public void test2() throws ParseException {
 		dateService.versionTest();
+	}
+
+	@GetMapping("/etc/pathTest")
+	public void test3() throws ParseException {
+		etcService.pathTest();
 	}
 }
---
+++ functionTest/src/main/java/com/function/functiontest/service/EtcService.java
@@ -0,0 +1,44 @@
+package com.function.functiontest.service;
+
+import java.io.File;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EtcService {
+
+	public void pathTest() {
+		// String sPath = System.getProperty("java.class.path");
+		// System.out.println(sPath);
+
+		String devPath = "file:/D:/RTD/project/dashboard-solution/dash-starter/dash-surface-starter/build/classes/java/main/com/nkia/starter/SurfaceStartApplication.class";
+		// String path = "file:/D:/RTD/project/dashboard-solution/dash-starter/dash-surface-starter/build/libs/dash-surface-starter.jar!/BOOT-INF/classes/com/nkia/starter/SurfaceStartApplication.class";
+		String jarPath = "jar:" + devPath.split("/classes")[0];
+
+		String projectHome = System.getProperty("user.dir");
+
+		String buildPath = projectHome + "/build/libs";
+
+		File jar = new File(buildPath);
+
+		String jarName = null;
+
+		for(String s : jar.list()) {
+			if(s.endsWith("Test.jar")) jarName = s;
+		}
+
+		System.out.println(buildPath + jarName + "!/BOOT-INF/classes/com");
+
+		jarPath = jarPath + jarName;
+
+		System.out.println("jarPath : " + jarPath);
+
+		String home = System.getProperty("user.dir");
+		System.out.println(System.getProperty("java.class.path"));
+		System.out.println(System.getProperty("user.dir"));
+	}
+}
