c6d3551097beabd65a85ac69cd8369d6d21c0910
WonTaeSeong <won4932@gmail.com>
2022-11-17 14:32:20 +0900

    junit test

---
+++ functionTest/src/main/java/com/function/functiontest/service/Calculator.java
@@ -0,0 +1,15 @@
+package com.function.functiontest.service;
+
+public class Calculator {
+	public int sum(int n1, int n2) {
+		return n1 + n2;
+	}
+
+	public int Multiply(int a, int b) {
+		return a * b;
+	}
+
+	public int Multiply(int a, int b, int c) {
+		return a * b * c;
+	}
+}
---
+++ functionTest/src/test/java/com/function/functiontest/service/CalculatorTest3.java
@@ -0,0 +1,16 @@
+package com.function.functiontest.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest3 {
+
+	@Test
+	void multiply() {
+	}
+
+	@Test
+	void testMultiply() {
+	}
+}
