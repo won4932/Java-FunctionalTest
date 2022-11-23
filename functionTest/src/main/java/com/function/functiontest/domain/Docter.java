7d7eae41576123dc2c44350e58da8e867ee6269d
WonTaeSeong <won4932@gmail.com>
2022-11-24 08:58:28 +0900

    calcuator test code

---
+++ functionTest/src/main/java/com/function/functiontest/domain/Docter.java
@@ -0,0 +1,33 @@
+package com.function.functiontest.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Docter {
+
+	@Id
+	private int id;
+
+	private String name;
+	private String sex;
+	private String phone;
+	private String email;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private MedicalDepartment medicalDepartmentId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Hospital hospitalId;
+}
