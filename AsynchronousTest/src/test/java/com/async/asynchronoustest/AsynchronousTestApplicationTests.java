89cac3388bea2aad6b38fd91b6cdab6366f2b069
WonTaeSeong <won4932@gmail.com>
2022-11-23 13:47:07 +0900

    Asynchronous test

---
+++ AsynchronousTest/src/test/java/com/async/asynchronoustest/AsynchronousTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.async.asynchronoustest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsynchronousTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
